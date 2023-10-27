3ecd698135568b2dad73e75a45d0a51d74b4f5d7
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-10-27 16:54:27 +0200

    Add: function for difficulty level WIP

--- js/script.js
+++ js/script.js
@@ -4,38 +4,58 @@ document.getElementById("btn").addEventListener("click" , gridMaker);
 
 const rowg = document.querySelector(".row");
 
+const difficultySelect = document.getElementById("difficultyLevel");
+
+// funzione con difficoltà
+
+function gridMaker() {
+    const difficulty = difficultySelect.value;
+    let DimensioneGriglia;
+
+    if (difficulty = "easy") {
+        DimensioneGriglia = 100; 
+
+    }else if (difficulty = "medium") {
+        DimensioneGriglia = 81;
+
+    }else if (difficulty = "hard") {
+        DimensioneGriglia = 49;
+    }
 
 
+}
 
 
 // FUNZIONE:
 
 // GRIGLIA
 
-function gridMaker() {
-    const rowg = document.querySelector(".row");
-    rowg.innerHTML= "";
+// function gridMaker() {
+//     const rowg = document.querySelector(".row");
+//     rowg.innerHTML= "";
 
-    for (let i = 1; i <= 100; i ++) {
-        let square = document.createElement("div");
-        square.className= `square`;
+
+//     for (let i = 1; i <= 100; i ++) {
+//         let square = document.createElement("div");
+//         square.className= `square`;
         
     
-        console.log(square);
+//         console.log(square);
 
-        square.addEventListener("click", function(){
-            colorSquare(square, i);
-        });
+//         square.addEventListener("click", function(){
+//             colorSquare(square, i);
+//         });
 
     
-        rowg.appendChild(square);
-    }
+//         rowg.appendChild(square);
+//     }
 
     
    
     
-}
+// }
 
+// COLORE + NUMERO AL CLICK
 
 function colorSquare(square, number) {
     square.textContent= number;
